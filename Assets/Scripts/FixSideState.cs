163b2a98772a055a964edb9cb83e1ba8b7e0a117
msgiven <marktodoroki2@gmail.com>
2025-11-01 23:24:28 +0300

    v.2 (#10)
    movement

---
+++ Assets/Scripts/FixSideState.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class FixSideState : State
+{
+    public FixSideState(Player player, StateMachine stateMachine, string animBoolName) : base(player, stateMachine, animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        Debug.Log("Side");
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
@@ -8,10 +9,46 @@ public class Player : MonoBehaviour
     StateMachine stateMachine;
     public IdleState idleState { get; private set; }
     public WalkState walkState { get; private set; }
+    public PutOutFireState putOutFireState { get; private set; }
+    public ShootState shootState { get; private set; }
+    public FixFloorState fixFloorState { get; private set; }
+    public FixSideState fixSideState { get; private set; }
 
     private Vector3 mousePos;
     public Vector3 targetPos { get; private set; }
     public float moveSpeed { get; private set; } = 5;
+    public string currentTag { get; private set; }
+
+
+    private void Awake()
+    {
+        targetPos = transform.position;
+        stateMachine = new StateMachine();
+        idleState = new IdleState(this, stateMachine, "IsIdle");
+        walkState = new WalkState(this, stateMachine, "IsWalk");
+        putOutFireState = new PutOutFireState(this, stateMachine, "IsPutOutFire");
+        shootState = new ShootState(this, stateMachine, "IsShoot");
+        fixFloorState = new FixFloorState(this, stateMachine, "IsFixFloor");
+        fixSideState = new FixSideState(this, stateMachine, "IsFixSide");
+    }
+    private void Start()
+    {
+        stateMachine.Initialize(idleState);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Gun") || collision.collider.CompareTag("SideHole")) { 
+        
+            //currentTag = "Gun";
+            targetPos = transform.position;
+        }
+        if (collision.collider.CompareTag("SideHole"))
+        {
+           /// currentTag = ""
+        }
+    }
 
     public Vector3 GetMousePos()
     {
@@ -22,6 +59,8 @@ public class Player : MonoBehaviour
     {
         mousePos = Input.mousePosition - GetMousePos();
     }
+
+
 
     /*private void OnMouseDrag()
     {
@@ -36,30 +75,27 @@ public class Player : MonoBehaviour
 
         if (plane.Raycast(ray, out distance))
         {
-            targetPos = ray.GetPoint(distance);
 
-            if (Vector3.Distance(transform.position, targetPos) > 0.1f)
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
             {
-                stateMachine.ChangeState(walkState);
+                if (hitInfo.collider.CompareTag("Gun") || hitInfo.collider.CompareTag("FloorHole") || hitInfo.collider.CompareTag("Fire") || hitInfo.collider.CompareTag("SideHole")) { 
+                    targetPos = ray.GetPoint(distance);
+                    currentTag = hitInfo.collider.tag;
+
+                    if (Vector3.Distance(transform.position, targetPos) > 0.1f)
+                    {
+                        stateMachine.ChangeState(walkState);
+                    }
+                }
+                
+                
             }
         }
-    }
-    private void Awake()
-    {
-        targetPos = transform.position;
-        stateMachine = new StateMachine();
-        idleState = new IdleState(this, stateMachine, "IsIdle");
-        walkState = new WalkState(this, stateMachine, "IsWalk");
-    }
-    private void Start()
-    {
-        stateMachine.Initialize(idleState);
     }
 
     private void Update()
     {
         stateMachine.currentState.Update();
-        //transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * moveSpeed);
 
     }
 
