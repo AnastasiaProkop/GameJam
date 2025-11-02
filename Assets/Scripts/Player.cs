48d5525dcc06bd6c5aa9e3f631834832ebf44916
Santalova Maria <marktodoroki2@gmail.com>
2025-11-02 18:28:48 +0300

    v.3
    анимация тентаклей

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,14 @@
+﻿using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.EventSystems;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class Player : MonoBehaviour
 {
 
-    StateMachine stateMachine;
+    public StateMachine stateMachine {  get; private set; }
     public IdleState idleState { get; private set; }
     public WalkState walkState { get; private set; }
     public PutOutFireState putOutFireState { get; private set; }
@@ -16,8 +18,14 @@ public class Player : MonoBehaviour
 
     private Vector3 mousePos;
     public Vector3 targetPos { get; private set; }
+
+    private Vector3 potentialPos;
+
+    [SerializeField] private GameObject dragedSailor;
     public float moveSpeed { get; private set; } = 5;
     public string currentTag { get; private set; }
+
+    public NavMeshAgent navMeshAgent { get; private set; }
 
 
     private void Awake()
@@ -30,24 +38,17 @@ public class Player : MonoBehaviour
         shootState = new ShootState(this, stateMachine, "IsShoot");
         fixFloorState = new FixFloorState(this, stateMachine, "IsFixFloor");
         fixSideState = new FixSideState(this, stateMachine, "IsFixSide");
+
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
     private void Start()
     {
         stateMachine.Initialize(idleState);
-    }
 
+        dragedSailor.SetActive(false);
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.CompareTag("Gun") || collision.collider.CompareTag("SideHole")) { 
-        
-            //currentTag = "Gun";
-            targetPos = transform.position;
-        }
-        if (collision.collider.CompareTag("SideHole"))
-        {
-           /// currentTag = ""
-        }
+        navMeshAgent.updateRotation = false; 
+        navMeshAgent.updateUpAxis = false;   
     }
 
     public Vector3 GetMousePos()
@@ -57,18 +58,34 @@ public class Player : MonoBehaviour
 
     private void OnMouseDown()
     {
+        dragedSailor.SetActive(true);
         mousePos = Input.mousePosition - GetMousePos();
     }
 
 
 
-    /*private void OnMouseDrag()
+    private void OnMouseDrag()
     {
-        targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePos);
-    }*/
+        potentialPos = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePos);
+
+        Plane plane = new Plane(Vector3.up, dragedSailor.transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float distance;
+
+        if (plane.Raycast(ray, out distance))
+        {            
+            potentialPos = ray.GetPoint(distance);
+            dragedSailor.transform.position = new Vector3(potentialPos.x, dragedSailor.transform.position.y, potentialPos.z);
+            
+        }
+
+
+    }
 
     private void OnMouseUp()
     {
+        dragedSailor.SetActive(false);
+
         Plane plane = new Plane(Vector3.up, transform.position);
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         float distance;
@@ -84,6 +101,7 @@ public class Player : MonoBehaviour
 
                     if (Vector3.Distance(transform.position, targetPos) > 0.1f)
                     {
+                        navMeshAgent.SetDestination(targetPos);
                         stateMachine.ChangeState(walkState);
                     }
                 }
