7e83ef81ed224e8e1068519fd3e0c2b28c1b3151
Anastasia Prokop <gpae98@gmail.com>
2025-11-03 17:48:42 +0300

    not pat attention

--- Assets/Scripts/TestChangeAtmosphere.cs
+++ Assets/Scripts/TestChangeAtmosphere.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 public class TestChangeAtmosphere : MonoBehaviour
@@ -54,6 +56,12 @@ public class TestChangeAtmosphere : MonoBehaviour
     [SerializeField, Range(0f, 5f)] private float globalIntensityNormal = 0.9f;
     [SerializeField, Range(0f, 5f)] private float globalIntensityMadness = 0.17f;
 
+    // global volume для затемнения вообще всего
+    [SerializeField] private Volume globalVolume;
+    [SerializeField] private UnityEngine.Color GlobalColorNormal = new UnityEngine.Color(1f, 1f, 1f, 1f);
+    [SerializeField] private UnityEngine.Color GlobalColorMadness = new UnityEngine.Color(0.7f, 0.8f, 1f, 1f);
+    private ColorAdjustments _colorAdj;
+
     void OnEnable()               
     {
         elapsed = 0f;
@@ -72,6 +80,20 @@ public class TestChangeAtmosphere : MonoBehaviour
         waterRenderer.SetPropertyBlock(null);
         Debug.Log($"MAT: {WaterMat.name} | SHADER: {WaterMat.shader.name}");
         DumpProps(WaterMat);
+
+        // проверка глобал вольюм на всякий
+        var profile = globalVolume.profile ?? globalVolume.sharedProfile;
+        if (!profile)
+        {
+            Debug.LogError("У Volume нет профиля.");
+            return;
+        }
+        if (!profile.TryGet(out _colorAdj))
+        {
+            _colorAdj = profile.Add<ColorAdjustments>(true); // true = сразу включить все overrides
+            Debug.LogWarning("В профиле не было Color Adjustments — добавили.");
+        }
+        _colorAdj.colorFilter.overrideState = true;
 
         //свет
         SetDay();
@@ -145,6 +167,9 @@ public class TestChangeAtmosphere : MonoBehaviour
 
         // 2d глобальный
         globalLight.intensity = globalIntensityNormal;
+
+        //глобал вольюм
+        _colorAdj.colorFilter.value = GlobalColorNormal;
     }
 
     void SetNight()
@@ -184,6 +209,8 @@ public class TestChangeAtmosphere : MonoBehaviour
         // 2d глобальный
         globalLight.intensity = globalIntensityMadness;
 
+        //глобал вольюм
+        _colorAdj.colorFilter.value = GlobalColorMadness;
     }
 
     void DumpProps(Material m)
